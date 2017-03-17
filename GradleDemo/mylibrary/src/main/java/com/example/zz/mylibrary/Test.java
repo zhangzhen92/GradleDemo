ca5c36f078b8e6857c54a06132238cc4bcf729c7
zz <zz_dazzling@163.com>
2017-03-17 18:30:24 +0800

    test

---
+++ GradleDemo/mylibrary/src/main/java/com/example/zz/mylibrary/Test.java
@@ -0,0 +1,14 @@
+package com.example.zz.mylibrary;
+
+/**
+ * 类描述：
+ * 创建人：zz
+ * 创建时间： 2017/3/17 18:26
+ */
+
+
+public class Test {
+    public double addSum(double numberFirst,double numberSecond){
+        return numberFirst + numberSecond;
+    }
+}
